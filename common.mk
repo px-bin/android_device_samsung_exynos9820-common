5cabc722561f77347d0922454cd4f7a4e810e418
pedro <pxd.bin@gmail.com>
2025-08-28 08:38:50 -0300

    Include Sony  Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby

--- common.mk
+++ common.mk
@@ -83,6 +83,9 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
